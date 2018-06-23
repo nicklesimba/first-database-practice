af23548a004d5449d340e8fe4164089cc39be3ab
nicklesimba <simha.nikhil@gmail.com>
2018-06-23 17:14:16 -0400

    code now updates an entry and deletes one also

--- src/main.go
+++ src/main.go
@@ -1,3 +1,5 @@
+//This is in its own branch just to test how to make a branch but yeah
+
 package main
 
 import (
@@ -25,17 +27,31 @@ func main() {
 	}
 	defer db.Close()
 
+	//This will update Jake Baldino and
+	//also will spit out the ID and email of the mentioned row
 	sqlStatement := `
-	INSERT INTO users (age, email, first_name, last_name)
-	VALUES ($1, $2, $3, $4)
-	RETURNING id`
-
-	id := 0
-	err = db.QueryRow(sqlStatement, 33, "asimha@gmail.com", "Ajay", "Simha").Scan(&id)
+	UPDATE users
+	SET first_name = $2, last_name = $3
+	WHERE id = $1
+	RETURNING id, email;
+	`
+	var email string
+	var id int
+	err = db.QueryRow(sqlStatement, 3, "Fake", "Baldino").Scan(&id, &email)
 	if err != nil {
-		fmt.Println(err)
+		panic(err)
+	}
+	fmt.Println("ID and email are:")
+	fmt.Println(id, email)
+
+	//Deleting Pappa's Yahoo account >:)
+	sqlStatement = `
+	DELETE FROM users
+	WHERE id = $1;
+	`
+	_, err = db.Exec(sqlStatement, 39)
+	if err != nil {
 		panic(err)
 	}
 
-	fmt.Println("New record ID is:", id)
 }
